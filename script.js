6ee1c578c8a8c793804913765f233ed868b66b8e
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-01-29 19:49:18 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
-const forecor = document.querySelector('.forecor');
-const forecorInput = document.querySelector('.forecor-input');
-const backcor = document.querySelector('.backcor');
-const backcorInput = document.querySelector('.backcor-input');
+const forecor = document.querySelector('.forecor'),
+    forecorInput = document.querySelector('.forecor-input'),
+    backcor = document.querySelector('.backcor'),
+    backcorInput = document.querySelector('.backcor-input');
 
-checker();
+forecorInput.value = forecor.value;
+backcorInput.value = backcor.value;
+checker(forecorInput.value, backcorInput.value);
 
 let selects = [forecor, backcor];
 selects.forEach(e => {
-    e.addEventListener('input', checker)
+    e.addEventListener('input', () => {
+        forecorInput.value = forecor.value;
+        backcorInput.value = backcor.value;
+        checker(forecorInput.value, backcorInput.value);
+
+    })
 })
 
-function checker() {
-    let Cor = forecor.value.slice(1, 7);
-    let Corfundo = backcor.value.slice(1, 7);
+let inputs = [forecorInput, backcorInput];
+inputs.forEach(e => {
+    e.addEventListener('change', () => {
+        forecor.value = forecorInput.value;
+        backcor.value = backcorInput.value;
+        checker(forecorInput.value, backcorInput.value);
 
-    fetch(`https://webaim.org/resources/contrastchecker/?fcolor=${Cor}&bcolor=${Corfundo}&api`)
+    })
+})
+
+function checker(cor, fundo) {
+    cor = forecor.value.slice(1, 7);
+    fundo = backcor.value.slice(1, 7);
+
+    fetch(`https://webaim.org/resources/contrastchecker/?fcolor=${cor}&bcolor=${fundo}&api`)
         .then(response => response.json())
         .then(r => {
             document.querySelector('.result p').innerText = r.ratio;
-            forecorInput.value = forecor.value;
-            backcorInput.value = backcor.value;
+            let normalAA = document.querySelector('.normal .aa'),
+                normalAAA = document.querySelector('.normal .aaa'),
+                largeAA = document.querySelector('.large .aa'),
+                largeAAA = document.querySelector('.large .aaa');
 
-            let normalAA = document.querySelector('.normal .aa');
-            let normalAAA = document.querySelector('.normal .aaa');
-            let largeAA = document.querySelector('.large .aa');
-            let largeAAA = document.querySelector('.large .aaa');
-
-            function texto() {
-                if (r.AA === 'pass') {
-                    normalAA.classList.add('passa');
-                } else {
-                    normalAA.classList.add('falha');
-                }
-
-                if (r.AAA === 'pass') {
-                    normalAAA.classList.add('passa');
-                } else {
-                    normalAAA.classList.add('falha');
-                }
-
-                if (r.AALarge === 'pass') {
-                    largeAA.classList.add('passa');
-                } else {
-                    largeAA.classList.add('falha');
-                }
-
-                if (r.AAALarge === 'pass') {
-                    largeAAA.classList.add('passa');
-                } else {
-                    largeAAA.classList.add('falha');
-                }
-            }
 
             let types = [normalAA, normalAAA, largeAA, largeAAA];
             types.forEach(e => {
                 e.classList.remove('passa', 'falha');
             })
-            texto();
+
+            function texto() {
+                r.AA == 'pass' ? normalAA.classList.add('passa') : normalAA.classList.add('falha');
+                r.AAA == 'pass' ? normalAAA.classList.add('passa') : normalAAA.classList.add('falha');
+
+                r.AALarge == 'pass' ? largeAA.classList.add('passa') : largeAA.classList.add('falha');
+                r.AAALarge == 'pass' ? largeAAA.classList.add('passa') : largeAAA.classList.add('falha');
+            }
+            texto()
         })
-
 }
-
-//https://webaim.org/resources/contrastchecker/?fcolor=fff&bcolor=000&api
